503dfc5877a0907fbb400bb9e8361838b59cb82d
hgao080 <hgao080@aucklanduni.ac.nz>
2024-07-14 11:02:45 +1200

    json-server database

--- carbon-calculator/src/Components/QuestionSlider.js
+++ carbon-calculator/src/Components/QuestionSlider.js
@@ -12,158 +12,179 @@ import questionSlidesData from '../data/questionSlides.json';
 // To change A QUESTION SLIDE go to questionSlides.json and edit the values
 
 const values = [
-    0.171, // Q1 multiplier value
-    0.4, // Q2 multiplier value - For this I did (250g / 5min x 8min)/1000, 8min is the average shower time 
-    0.0314, // Q3 multiplier value
-    154, // Q4 multiplier value - For this I did (175g/km x 900km/h)
-    0.0938, // Q5 multiplier value - For this I did (108.46g/km x 50km/h x 1h/60min)  / 1000
-    0.0792, // Q6 multiplier value - For this I did (35.46g/km x 120km/h x 1h/60min) / 1000
-    0.062, // Q7 mutliplier value 
+  0.171, // Q1 multiplier value
+  0.4, // Q2 multiplier value - For this I did (250g / 5min x 8min)/1000, 8min is the average shower time 
+  0.0314, // Q3 multiplier value
+  154, // Q4 multiplier value - For this I did (175g/km x 900km/h)
+  0.0938, // Q5 multiplier value - For this I did (108.46g/km x 50km/h x 1h/60min)  / 1000
+  0.0792, // Q6 multiplier value - For this I did (35.46g/km x 120km/h x 1h/60min) / 1000
+  0.062, // Q7 mutliplier value 
 ]; // Constant multipliers
 
 const questionSlides = questionSlidesData.questionSlides;
 
 export default function QuestionSlider() {
 
-    const [currentQuestionIndex, setcurrentQuestionIndex] = useState(0);
-    const [inputs, setInputs] = useState(new Array(questionSlides.length).fill(''));
-    const [results, setResults] = useState(new Array(questionSlides.length).fill(''));
-    const [inputErrors, setInputErrors] = useState(new Array(questionSlides.length).fill(false));
-    const [totalResult, setTotalResult] = useState(0);
+  const [currentQuestionIndex, setcurrentQuestionIndex] = useState(0);
+  const [inputs, setInputs] = useState(new Array(questionSlides.length).fill(''));
+  const [results, setResults] = useState(new Array(questionSlides.length).fill(''));
+  const [inputErrors, setInputErrors] = useState(new Array(questionSlides.length).fill(false));
+  const [totalResult, setTotalResult] = useState(0);
+  const [currentDate, setCurrentDate] = useState(getDate());
+  const [JSONData, setJSONData] = useState(null)
 
-    const nextQuestion = () => {
-        setcurrentQuestionIndex(currentQuestionIndex === questionSlides.length - 1 ? 0 : currentQuestionIndex + 1);
-        handleCalculate()
-    }
+  function getDate() {
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    const date = today.getDate();
+    return `${month}/${date}/${year}`;
+  }
 
-    const prevQuestion = () => {
-        setcurrentQuestionIndex(currentQuestionIndex === 0 ? questionSlides.length - 1 : currentQuestionIndex - 1)
-    }
+  const nextQuestion = () => {
+    setcurrentQuestionIndex(currentQuestionIndex === questionSlides.length - 1 ? 0 : currentQuestionIndex + 1);
+    handleCalculate()
+  }
 
-    const handleInputChange = (event) => { 
-        const { value } = event.target;
+  const prevQuestion = () => {
+    setcurrentQuestionIndex(currentQuestionIndex === 0 ? questionSlides.length - 1 : currentQuestionIndex - 1)
+  }
 
-        const isValidInput = /^\d+$/.test(value);
+  const handleInputChange = (event) => {
+    const { value } = event.target;
 
-        const newInputs = [...inputs];
+    const isValidInput = /^\d+$/.test(value);
+
+    const newInputs = [...inputs];
     if (value === '') {
-        newInputs[currentQuestionIndex] = '';
-    } 
+      newInputs[currentQuestionIndex] = '';
+    }
     else {
-        newInputs[currentQuestionIndex] = value;
+      newInputs[currentQuestionIndex] = value;
     }
     setInputs(newInputs);
 
-        const newInputErrors = [...inputErrors];
-        newInputErrors[currentQuestionIndex] = !isValidInput;
-        setInputErrors(newInputErrors);
+    const newInputErrors = [...inputErrors];
+    newInputErrors[currentQuestionIndex] = !isValidInput;
+    setInputErrors(newInputErrors);
+  }
+
+  const handleCalculate = () => {
+    const inputValue = parseInt(inputs[currentQuestionIndex]);
+    const result = inputValue * values[currentQuestionIndex];
+    const newResults = [...results];
+    newResults[currentQuestionIndex] = result;
+    setResults(newResults);
+
+    if (currentQuestionIndex == 6) {
+      setJSONData({ currentDate, results})
+      fetch("http://localhost:8000/results/", {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(JSONData)
+      }).then(() => {
+        console.log(JSONData);
+      })
     }
+  }
 
-    const handleCalculate = () => {
-        const inputValue = parseInt(inputs[currentQuestionIndex]);
-        const result = inputValue * values[currentQuestionIndex];
-        const newResults = [...results];
-        newResults[currentQuestionIndex] = result;
-        setResults(newResults);
-        console.log(results);
-    }
+  useEffect(() => {
+    const newTotalResult = results.reduce((acc, curr) => {
+      const parsedResult = parseInt(curr);
+      return acc + (isNaN(parsedResult) ? 0 : parsedResult);
+    }, 0);
+    setTotalResult(newTotalResult);
+  }, [results]);
 
-    useEffect(() => {
-        const newTotalResult = results.reduce((acc, curr) => {
-            const parsedResult = parseInt(curr);
-            return acc + (isNaN(parsedResult) ? 0 : parsedResult);
-        }, 0);
-        setTotalResult(newTotalResult);
-    }, [results]);
+  const handleSubmit = () => {
+    handleCalculate();
+    nextQuestion();
+  }
 
-    const handleSubmit = () => {
-        handleCalculate();
-        nextQuestion();
-    }
-    
-    const theme = createTheme()
+  const theme = createTheme()
 
-    theme.typography.h3 = {
-        fontWeight: '600',
-        fontSize: '50px',
-        color: 'white'
-    }
+  theme.typography.h3 = {
+    fontWeight: '600',
+    fontSize: '50px',
+    color: 'white'
+  }
 
 
-    return (
+  return (
 
-        <div className={questionSliderStyle.questionSliderContainer}>
+    <div className={questionSliderStyle.questionSliderContainer}>
 
-            <div className={questionSliderStyle.questionSlides}>
-                {questionSlides.map((questionSlide, index) => {
-                    return (
-                        <div className={ currentQuestionIndex === index ? questionSliderStyle.questionVisible : questionSliderStyle.questionHidden } 
-                            style={{
-                                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('${questionSlide.imgUrl}')`,
-                                backgroundSize: 'cover'}}
-                        >
-                            <Typography variant="h3" color="white" className={questionSliderStyle.header}>
-                                {questionSlide.question}
-                            </Typography>
+      <div className={questionSliderStyle.questionSlides}>
+        {questionSlides.map((questionSlide, index) => {
+          return (
+            <div className={currentQuestionIndex === index ? questionSliderStyle.questionVisible : questionSliderStyle.questionHidden}
+              style={{
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('${questionSlide.imgUrl}')`,
+                backgroundSize: 'cover'
+              }}
+            >
+              <Typography variant="h3" color="white" className={questionSliderStyle.header}>
+                {questionSlide.question}
+              </Typography>
 
-                            <div className={ index === questionSlides.length - 1 ? questionSliderStyle.finalResult : questionSliderStyle.hidden }>
-                                <ThemeProvider theme={theme}>
-                                    <Typography variant="h3" className={questionSliderStyle.finalHeader}>
-                                        You weekly contribution is:
-                                    </Typography>
-                                    <Typography variant="h3" className={questionSliderStyle.finalResult}>
-                                        {totalResult}kg
-                                    </Typography>
-                                </ThemeProvider>
-                            </div>
+              <div className={index === questionSlides.length - 1 ? questionSliderStyle.finalResult : questionSliderStyle.hidden}>
+                <ThemeProvider theme={theme}>
+                  <Typography variant="h3" className={questionSliderStyle.finalHeader}>
+                    You weekly contribution is:
+                  </Typography>
+                  <Typography variant="h3" className={questionSliderStyle.finalResult}>
+                    {totalResult}kg
+                  </Typography>
+                </ThemeProvider>
+              </div>
 
-                            <Box component="form" sx={{'& > :not(style)': { m: 1, width: '400px', border: '0' }, }} noValidate autoComplete="off" className={ index === questionSlides.length - 1 ? questionSliderStyle.hidden : questionSliderStyle.visible }>
-                                <TextField 
-                                    id="outlined basic" 
-                                    label="Enter a number" 
-                                    variant="outlined" 
-                                    style={{color:"white", border: 'none'}}
-                                    onChange={handleInputChange}
-                                    value={inputs[currentQuestionIndex]}
-                                    error={inputErrors[index]}
-                                    helperText={inputErrors[index] ? 'Please enter a number' : ''}
-                                    className={questionSliderStyle.textField}
-                                />
-                            </Box>
+              <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '400px', border: '0' }, }} noValidate autoComplete="off" className={index === questionSlides.length - 1 ? questionSliderStyle.hidden : questionSliderStyle.visible}>
+                <TextField
+                  id="outlined basic"
+                  label="Enter a number"
+                  variant="outlined"
+                  style={{ color: "white", border: 'none' }}
+                  onChange={handleInputChange}
+                  value={inputs[currentQuestionIndex]}
+                  error={inputErrors[index]}
+                  helperText={inputErrors[index] ? 'Please enter a number' : ''}
+                  className={questionSliderStyle.textField}
+                />
+              </Box>
 
-                            <div className={questionSliderStyle.navigators}>
-                                <div className={questionSliderStyle.buttons}>
-                                    <Button variant="contained" onClick={prevQuestion}>
-                                        prev
-                                    </Button>
+              <div className={questionSliderStyle.navigators}>
+                <div className={questionSliderStyle.buttons}>
+                  <Button variant="contained" onClick={prevQuestion}>
+                    prev
+                  </Button>
 
-                                    {index < questionSlides.length - 2 && ( 
-                                        <Button variant="contained" onClick={nextQuestion}>
-                                            Next
-                                        </Button>
-                                    )}
+                  {index < questionSlides.length - 2 && (
+                    <Button variant="contained" onClick={nextQuestion}>
+                      Next
+                    </Button>
+                  )}
 
-                                    {index === questionSlides.length - 2 && ( 
-                                        <Button variant="contained" onClick={handleSubmit}>
-                                            Submit
-                                        </Button>
-                                    )}
-                                </div>
+                  {index === questionSlides.length - 2 && (
+                    <Button variant="contained" onClick={handleSubmit}>
+                      Submit
+                    </Button>
+                  )}
+                </div>
 
-                                <span className={questionSliderStyle.indicatorContainer}>
-                                    {questionSlides.map((_, index) => {
-                                        return <button 
-                                            key={index} 
-                                            onClick={() => setcurrentQuestionIndex(index)} 
-                                            className={currentQuestionIndex >= index ? questionSliderStyle.activeIndicator : questionSliderStyle.inactiveIndicator}
-                                        ></button>
-                                    })}
-                                </span>
-                            </div>
-                        </div>
-                    )
-                })}
+                <span className={questionSliderStyle.indicatorContainer}>
+                  {questionSlides.map((_, index) => {
+                    return <button
+                      key={index}
+                      onClick={() => setcurrentQuestionIndex(index)}
+                      className={currentQuestionIndex >= index ? questionSliderStyle.activeIndicator : questionSliderStyle.inactiveIndicator}
+                    ></button>
+                  })}
+                </span>
+              </div>
             </div>
-        </div>
-    )
+          )
+        })}
+      </div>
+    </div>
+  )
 }
--- carbon-calculator/src/Pages/DashboardImprovement.js
+++ carbon-calculator/src/Pages/DashboardImprovement.js
@@ -4,44 +4,74 @@ import Sidebar from "../Components/Sidebar";
 import SuggestionList from "../Components/SuggestionList";
 import Logo from "../Components/Logo";
 
-const avgValues = [192, 12, 42, 0, 15]
-
 const DashboardImprovement = () => {
-  const [suggestions, setSuggestions] = useState([])
-  const results = ["200", "15", "44", "2", "12"]
+  const avgValues = [32.832, 2.8, 1.3188, 0, 1.407, 1.188, 62]
+
+  const[date, setDate] = useState(getDate())
+  const [suggestions, setSuggestions] = useState(null)
+  const [results, setResults] = useState([])
+
+  function getDate() {
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    const date = today.getDate();
+    return `${month}/${date}/${year}`;
+  }
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/results');
+        const data = await res.json();
+        const correctObject = data[data.length - 1];
+        setResults(correctObject.results);
+      } catch (error) {
+        console.error("Failed to fetch results:", error);
+      }
+    };
+  
+    fetchData();
+  }, [])
+
+  useEffect(() => {
+    // Assuming avgValues is available in the component's scope
     const updatedSuggestions = results.map((result, index) => {
       const avg = avgValues[index];
       const res = parseInt(result, 10);
+      console.log(result)
+      console.log(avg)
+      console.log(index)
 
-      if (index == 4) {
-        if (res < avg) {
-          return "AT Transport ain't that bad"
-        }
+      if (index === 3 && result < avg) {
+        return "AT Transport ain't that bad, though it is expensive now";
       }
 
-      if (res > avg) {
+      if (result > avg) {
         switch (index) {
           case 0:
-            return "Consider driving less and utilising other means of transport such as walking, biking and public transport"
+            return "Consider driving less and utilising other means of transport such as walking, biking and public transport";
           case 1:
-            return "You're taking too long to shower buddy. How about hurrying up, the water ain't free"
+            return "1 shower a day is plenty but more doesn't hurt that much I guess. Just don't stay in there for too long";
           case 2:
-            return "Man you rotted really badly this week. Go outside and touch some grass"
+            return "Holy screentime dude. How about going outside or meeting up with some friends";
           case 3:
-            return "Why'd you hop on a flight dude, could've just swam"
+            return "Couple of flights aren't that bad for you, but they're bad for the environment";
           case 4:
-            return ""
+            return "Utilising AT's horrible transport I like it";
+          case 5:
+            return "To be fair the trains actually aren't that bad";
+          case 6:
+            return "Holy fatty eating like a cow";
           default:
-            return "The coders are useless"
+            return "The coders are useless";
         }
       }
       return "";
     }).filter(suggestion => suggestion !== "");
 
     setSuggestions(updatedSuggestions);
-  }, [])
+  }, [results])
 
 
   return (
@@ -58,7 +88,7 @@ const DashboardImprovement = () => {
 
         <div className={styles.suggestionsTitle}>Today's Suggestions</div>
         <div className={styles.improvements}>
-          <SuggestionList suggestions={suggestions} />
+          {suggestions === null ? <div></div> : <SuggestionList suggestions={suggestions} />}
         </div>
       </div>
     </div>
