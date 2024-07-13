b3b4f7925bea909f1cba313a957600ea3c3c91e2
Jin <ajhz011@aucklanduni.ac.nz>
2024-07-13 21:21:03 +1200

    Added a background image to all the slides

--- carbon-calculator/src/Components/QuestionSlider.js
+++ carbon-calculator/src/Components/QuestionSlider.js
@@ -1,30 +1,30 @@
 import questionSliderStyle from '../ComponentStyles/QuestionSlider.module.css';
-import questionsArr from '../data/Questions.json';
 import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Link } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import questionSlidesData from '../data/questionSlides.json';
 
 const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Constant multipliers
+const questionSlides = questionSlidesData.questionSlides;
 
 export default function QuestionSlider() {
 
     const [currentQuestionIndex, setcurrentQuestionIndex] = useState(0);
-    const [inputs, setInputs] = useState(new Array(questionsArr.length).fill(''));
-    const [results, setResults] = useState(new Array(questionsArr.length).fill(''));
-    const [inputErrors, setInputErrors] = useState(new Array(questionsArr.length).fill(false));
+    const [inputs, setInputs] = useState(new Array(questionSlides.length).fill(''));
+    const [results, setResults] = useState(new Array(questionSlides.length).fill(''));
+    const [inputErrors, setInputErrors] = useState(new Array(questionSlides.length).fill(false));
     const [totalResult, setTotalResult] = useState(0);
 
     const nextQuestion = () => {
-        setcurrentQuestionIndex(currentQuestionIndex === questionsArr.length - 1 ? 0 : currentQuestionIndex + 1);
+        setcurrentQuestionIndex(currentQuestionIndex === questionSlides.length - 1 ? 0 : currentQuestionIndex + 1);
         handleCalculate()
     }
 
     const prevQuestion = () => {
-        setcurrentQuestionIndex(currentQuestionIndex === 0 ? questionsArr.length - 1 : currentQuestionIndex - 1)
+        setcurrentQuestionIndex(currentQuestionIndex === 0 ? questionSlides.length - 1 : currentQuestionIndex - 1)
     }
 
     const handleInputChange = (event) => { 
@@ -82,25 +82,29 @@ export default function QuestionSlider() {
         <div className={questionSliderStyle.questionSliderContainer}>
 
             <div className={questionSliderStyle.questionSlides}>
-                {questionsArr.map((question, index) => {
+                {questionSlides.map((questionSlide, index) => {
                     return (
-                        <div className={ currentQuestionIndex === index ? questionSliderStyle.questionVisible : questionSliderStyle.questionHidden }>
+                        <div className={ currentQuestionIndex === index ? questionSliderStyle.questionVisible : questionSliderStyle.questionHidden } 
+                            style={{
+                                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('${questionSlide.imgUrl}')`,
+                                backgroundSize: 'cover'}}
+                        >
                             <Typography variant="h3" color="white" className={questionSliderStyle.header}>
-                                {question}
+                                {questionSlide.question}
                             </Typography>
 
-                            <div className={ index === questionsArr.length - 1 ? questionSliderStyle.finalResult : questionSliderStyle.hidden }>
+                            <div className={ index === questionSlides.length - 1 ? questionSliderStyle.finalResult : questionSliderStyle.hidden }>
                                 <ThemeProvider theme={theme}>
                                     <Typography variant="h3" className={questionSliderStyle.finalHeader}>
                                         You weekly contribution is:
                                     </Typography>
-                                    <Typography variant="h3" className={questionSliderStyle.finalRe}>
+                                    <Typography variant="h3" className={questionSliderStyle.finalResult}>
                                         {totalResult}kg
                                     </Typography>
                                 </ThemeProvider>
                             </div>
 
-                            <Box component="form" sx={{'& > :not(style)': { m: 1, width: '400px', border: '0' }, }} noValidate autoComplete="off" className={ index === questionsArr.length - 1 ? questionSliderStyle.hidden : questionSliderStyle.visible }>
+                            <Box component="form" sx={{'& > :not(style)': { m: 1, width: '400px', border: '0' }, }} noValidate autoComplete="off" className={ index === questionSlides.length - 1 ? questionSliderStyle.hidden : questionSliderStyle.visible }>
                                 <TextField 
                                     id="outlined basic" 
                                     label="Enter a number" 
@@ -114,37 +118,39 @@ export default function QuestionSlider() {
                                 />
                             </Box>
 
-                            <div className={questionSliderStyle.buttons}>
-                                <Button variant="contained" onClick={prevQuestion}>
-                                    prev
-                                </Button>
-
-                                {index < questionsArr.length - 2 && ( 
-                                    <Button variant="contained" onClick={nextQuestion}>
-                                        Next
+                            <div className={questionSliderStyle.navigators}>
+                                <div className={questionSliderStyle.buttons}>
+                                    <Button variant="contained" onClick={prevQuestion}>
+                                        prev
                                     </Button>
-                                )}
 
-                                {index === questionsArr.length - 2 && ( 
-                                    <Button variant="contained" onClick={handleSubmit}>
-                                        Submit
-                                    </Button>
-                                )}
+                                    {index < questionSlides.length - 2 && ( 
+                                        <Button variant="contained" onClick={nextQuestion}>
+                                            Next
+                                        </Button>
+                                    )}
+
+                                    {index === questionSlides.length - 2 && ( 
+                                        <Button variant="contained" onClick={handleSubmit}>
+                                            Submit
+                                        </Button>
+                                    )}
+                                </div>
+
+                                <span className={questionSliderStyle.indicatorContainer}>
+                                    {questionSlides.map((_, index) => {
+                                        return <button 
+                                            key={index} 
+                                            onClick={() => setcurrentQuestionIndex(index)} 
+                                            className={currentQuestionIndex >= index ? questionSliderStyle.activeIndicator : questionSliderStyle.inactiveIndicator}
+                                        ></button>
+                                    })}
+                                </span>
                             </div>
                         </div>
                     )
                 })}
             </div>
-
-            <span className={questionSliderStyle.indicatorContainer}>
-                {questionsArr.map((_, index) => {
-                    return <button 
-                        key={index} 
-                        onClick={() => setcurrentQuestionIndex(index)} 
-                        className={currentQuestionIndex >= index ? questionSliderStyle.activeIndicator : questionSliderStyle.inactiveIndicator}
-                    ></button>
-                })}
-            </span>
         </div>
     )
 }
