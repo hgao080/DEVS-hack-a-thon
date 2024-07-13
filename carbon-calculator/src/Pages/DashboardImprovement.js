2490b7e866281857298a55dec6ff3c51a4d3c205
hgao080 <hgao080@aucklanduni.ac.nz>
2024-07-13 17:06:13 +1200

    ok

--- carbon-calculator/src/Pages/DashboardImprovement.js
+++ carbon-calculator/src/Pages/DashboardImprovement.js
@@ -4,16 +4,22 @@ import Sidebar from "../Components/Sidebar";
 import SuggestionList from "../Components/SuggestionList";
 import Logo from "../Components/Logo";
 
-const avgValues = [192, 12, 42, 0]
+const avgValues = [192, 12, 42, 0, 15]
 
 const DashboardImprovement = () => {
   const [suggestions, setSuggestions] = useState([])
-  const results = ["100", "15", "44", "2"]
+  const results = ["100", "15", "44", "2", "17"]
 
   useEffect(() => {
     const updatedSuggestions = results.map((result, index) => {
       const avg = avgValues[index];
       const res = parseInt(result, 10);
+
+      if (index == 4) {
+        if (res < avg) {
+          return "AT Transport ain't that bad"
+        }
+      }
 
       if (res > avg) {
         switch (index) {
@@ -25,6 +31,8 @@ const DashboardImprovement = () => {
             return "Man you rotted really badly this week. Go outside and touch some grass"
           case 3:
             return "Why'd you hop on a flight dude, could've just swam"
+          case 4:
+            return ""
           default:
             return "The coders are useless"
         }
